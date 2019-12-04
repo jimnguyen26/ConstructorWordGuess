7f884370714a72394413fe36b8c9ca3be47c407a
Jim <jimmybnguyen@yahoo.com>
2019-12-04 15:28:58 -0800

    teams added and some variables

--- index.js
+++ index.js
@@ -4,3 +4,40 @@ const prompt = require('prompt');
 const inquirer = require('inquirer');
 const gi = require(`gitignore`);
 const Word = require("./Word");
+
+const westernTeams = [
+"Nuggets", "Jazz", "Timberwolves", "Thunder", "Trailblazers", 
+"Lakers", "Clippers", "Suns", "Kings", "Warriors", 
+"Mavericks", "Rockets", "Spurs", "Grizzles", "Pelicans",
+"Raptors", "Celtics", "76ers", "Nets", "Knicks",
+"Bucks", "Pacers", "Pistons", "Bulls", "Cavaliers",
+"Heat", "Magic", "Hornets", "Wizards", "Hawks"
+];
+
+console.log(westernTeams);
+
+let guess;
+let chosenWords;
+let word;
+let chosenLetter;
+
+function runIt() {
+    chosenWords = [];
+    console.log("Welcome to the NBA guessing game where you guess the team!");
+};
+
+function playGame() {
+    chosenLetter = "";
+    guess = 12;
+    if (chosenWords.length < westernTeams.length) {
+        chosenLetter = getLetter();
+    } else {
+        console.log("You sure know your NBA teams!");
+        continuePrompt();
+    }
+    if (chosenLetter) {
+        word = new Word(chosenLetter);
+        word.makeLetters();
+        makeGuess();
+    }
+}
